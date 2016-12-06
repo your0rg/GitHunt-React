43d057de767158d947e91712b77227b54ef43c9e
Sashko Stubailo <sashko@stubailo.com>
2016-12-05 16:02:20 -0800

    Make a lot of progress on the store inspector

--- ui/inspector/index.js
+++ ui/inspector/index.js
@@ -7,40 +7,115 @@ export default class Inspector extends React.Component {
 
   render() {
     return (
-      <div>
-        <StoreTreeFieldSet data={this.getData()} dataId="ROOT_QUERY" />
-      </div>
+      <AllRealIds data={this.getData()} />
     );
   }
 }
 
-// Props: data, dataId
+const AllRealIds = ({ data }) => {
+  const ids = Object.keys(data).filter(id => id[0] !== '$');
+
+  const sortedIdsWithoutRoot = ids.filter(id => id !== 'ROOT_QUERY').sort();
+
+  // XXX handle root mutation and subscription fields as well
+  const rootFirst = ['ROOT_QUERY', ...sortedIdsWithoutRoot];
+
+  return (
+    <div>
+      {rootFirst.map(id => <StoreTreeFieldSet data={data} dataId={id} expand={false} />)}
+    </div>
+  );
+}
+
+// Props: data, dataId, expand
 class StoreTreeFieldSet extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      expand: props.expand || props.dataId[0] === '$',
+    };
+
+    this.toggleExpand = this.toggleExpand.bind(this);
+  }
+
   getStoreObj() {
     return this.props.data[this.props.dataId];
   }
 
-  render() {
+  shouldDisplayId() {
+    return this.props.dataId[0] !== '$';
+  }
+
+  keysToDisplay() {
+    return Object.keys(this.getStoreObj()).sort();
+  }
+
+  renderFieldSet() {
     const storeObj = this.getStoreObj();
+
+    return (
+      <div className="store-tree-field-set">
+        {this.keysToDisplay().map((key) => (
+          <StoreTreeField
+            data={this.props.data}
+            storeKey={key}
+            value={storeObj[key]}
+          />
+        ))}
+      </div>
+    )
+  }
+
+  toggleExpand() {
+    this.setState(({ expand }) => ({ expand: !expand }));
+  }
+
+  render() {
     return (
       <div>
-        {this.props.dataId}
-        <div className="store-tree-field-set">
-          {Object.keys(storeObj).map((key) => (
-            <StoreTreeField
-              data={this.props.data}
-              storeKey={key}
-              value={storeObj[key]}
-            />
-          ))}
-        </div>
+        {this.shouldDisplayId() && (
+          <span onClick={this.toggleExpand} className="store-tree-ref-id">
+            {this.state.expand ? '- ' : '+ '}
+            {this.props.dataId}
+          </span>
+        )}
+        {this.state.expand && this.renderFieldSet()}
       </div>
     )
   }
 }
 
-class StoreTreeArray extends React.Component {
+const StoreTreeArray = ({ value, data }) => (
+  <div>
+    {value.map(item => <StoreTreeValue value={item} data={data} /> )}
+  </div>
+)
 
+const StoreTreeObject = ({ value, data }) => {
+  if (isIdReference(value)) {
+    console.log(value);
+    return (
+      <StoreTreeFieldSet dataId={value.id} data={data} />
+    )
+  }
+
+  return (
+    <span>{JSON.stringify(value)}</span>
+  );
+}
+
+// props: data, value
+class StoreTreeValue extends React.Component {
+  render() {
+    return (
+      <span>
+        {Array.isArray(this.props.value) ?
+          <StoreTreeArray {...this.props} /> :
+          <StoreTreeObject {...this.props} />
+        }
+      </span>
+    )
+  }
 }
 
 // Props: data, storeKey, value
@@ -48,9 +123,14 @@ class StoreTreeField extends React.Component {
   render() {
     return (
       <div>
-        {this.props.storeKey}
-        {JSON.stringify(this.props.value, null, 2)}
+        {this.props.storeKey}:{" "}
+        <StoreTreeValue value={this.props.value} data={this.props.data} />
       </div>
     )
   }
+}
+
+// Should be imported from AC
+function isIdReference(storeObj) {
+  return storeObj && storeObj.type === 'id';
 }
